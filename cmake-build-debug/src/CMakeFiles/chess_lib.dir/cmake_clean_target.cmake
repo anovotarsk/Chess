dffdc78defd60227fbf8bd7cf973a4fdb189738d
anovotarsk <novotarskiy30112000@gmail.com>
2020-11-18 10:11:04 +0200

    fix

--- cmake-build-debug/src/CMakeFiles/chess_lib.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libchess_lib.a"
-)
--- src/chessboard.h
+++ src/chessboard.h
@@ -15,19 +15,19 @@
 #include <algorithm>
 #include <QMessageBox>
 
-#define WHITE_KING      "resources\\king_white.png"
-#define WHITE_QUEEN     "resources\\queen_white.png"
-#define WHITE_ROOK      "resources\\rook_white.png"
-#define WHITE_KNIGHT    "resources\\knight_white.png"
-#define WHITE_BISHOP    "resources\\bishop_white.png"
-#define WHITE_PAWN      "resources\\pawn_white.png"
+#define WHITE_KING      "resources/king_white.png"
+#define WHITE_QUEEN     "resources/queen_white.png"
+#define WHITE_ROOK      "resources/rook_white.png"
+#define WHITE_KNIGHT    "resources/knight_white.png"
+#define WHITE_BISHOP    "resources/bishop_white.png"
+#define WHITE_PAWN      "resources/pawn_white.png"
 
-#define BLACK_KING      "resources\\king_black.png"
-#define BLACK_QUEEN     "resources\\queen_black.png"
-#define BLACK_ROOK      "resources\\rook_black.png"
-#define BLACK_KNIGHT    "resources\\knight_black.png"
-#define BLACK_BISHOP    "resources\\bishop_black.png"
-#define BLACK_PAWN      "resources\\pawn_black.png"
+#define BLACK_KING      "resources/king_black.png"
+#define BLACK_QUEEN     "resources/queen_black.png"
+#define BLACK_ROOK      "resources/rook_black.png"
+#define BLACK_KNIGHT    "resources/knight_black.png"
+#define BLACK_BISHOP    "resources/bishop_black.png"
+#define BLACK_PAWN      "resources/pawn_black.png"
 
 class ChessBoard : public QWidget {
     Q_OBJECT
